64c3be48ded43dd9b874891d179bde329e632161
Felipe Homrich Melchior <fehmel@gmail.com>
2019-08-20 10:54:52 -0300

    feat: add password_hash

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/user';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Felipe Melchior',
-    email: 'fehmel@gmail.com',
-    password_hash: '123123',
-  });
+routes.post('/users', UserController.store);
 
-  return res.json(user);
-});
 export default routes;
