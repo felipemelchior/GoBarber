683cb6422eae653cf42cc77f271dbeb9542ea7b7
Felipe Homrich Melchior <fehmel@gmail.com>
2019-08-20 10:34:20 -0300

    feat: db migrate/create user

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/user';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'hello world' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Felipe Melchior',
+    email: 'fehmel@gmail.com',
+    password_hash: '123123',
+  });
 
+  return res.json(user);
+});
 export default routes;
