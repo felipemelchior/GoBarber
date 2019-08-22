93a060682531bb23ddc27a548f90c99da1012378
Felipe Homrich Melchior <fehmel@gmail.com>
2019-08-22 10:46:35 -0300

    feat: add migrations appointments

--- src/app/models/file.js
+++ src/app/models/file.js
@@ -6,6 +6,12 @@ class File extends Model {
       {
         name: Sequelize.STRING,
         path: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `http://localhost:9091/files/${this.path}`;
+          },
+        },
       },
       {
         sequelize,
