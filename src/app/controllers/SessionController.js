c99e0dc5b2197cc47a96348a1d1ec1b292cdb72d
Felipe Homrich Melchior <fehmel@gmail.com>
2019-10-11 11:43:26 -0300

    feat: add changes to api

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -3,6 +3,8 @@ import * as Yup from 'yup';
 import User from '../models/user';
 import authConfig from '../../config/auth';
 
+import File from '../models/file';
+
 class SessionController {
   async store(req, res) {
     const schema = Yup.object().shape({
@@ -18,7 +20,16 @@ class SessionController {
 
     const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     if (!user) {
       return res.status(401).json({ error: 'User not found' });
@@ -28,13 +39,15 @@ class SessionController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name } = user;
+    const { id, name, avatar, provider } = user;
 
     return res.json({
       user: {
         id,
         name,
         email,
+        avatar,
+        provider,
       },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
